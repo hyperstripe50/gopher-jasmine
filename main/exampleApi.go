d7b1fcfffe86556042698b4d41504f22df887eee
Jonathan Scott <jona@Jonathans-MBP.fios-router.home>
2021-02-07 12:45:18 -0500

    enhance example api

--- main/exampleApi.go
+++ main/exampleApi.go
@@ -9,6 +9,12 @@ func main() {
 	s := suite.NewSynchronousSuite("parent suite").
 		It("should run one top level test", func(instance map[string]interface{}) error {
 			return nil
+		}).
+		Describe("child suites", func() suite.Suite {
+			return suite.NewSynchronousSuite("first child suite").
+				It("should run one child suite", func(instance map[string]interface{}) error {
+					return nil
+				})
 		})
 	api.NewApi([]suite.Suite{s}).ListenAndServe(":9091")
 }
